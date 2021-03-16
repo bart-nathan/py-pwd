1ea91665c30819628cd616f1139f047cbed86099
bart-nathan <johnson_bart@protonmail.ch>
2021-03-16 09:45:53 +0100

    tir 16 mar 09:45:53 CET 2021

---
+++ pwm.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+
+if __name__ == "__main__":
+
+
+    try:
+        
+    command = sys.argv[1]
+
+    if command == "l":
+
+        print("list records")
+
+    except:
+        print("error :-)")
+
+    
+
+ 
+
+    
+    
+    
+    
+    print("Ok\n");
---
+++ pwm.sql
@@ -0,0 +1,9 @@
+create table record (
+
+    it integer not null primary key autoincrement,
+    sitename varchar(100) not null,
+    username varchar(100) not null,
+    password varchar(100) not null,
+    link varchar(300) not null
+
+);
