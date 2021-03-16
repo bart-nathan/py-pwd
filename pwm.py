b6ead443537434bf4ae055c14a8eebb354045a38
bart-nathan <johnson_bart@protonmail.ch>
2021-03-16 09:53:44 +0100

    tir 16 mar 09:53:44 CET 2021

--- pwm.py
+++ pwm.py
@@ -6,14 +6,28 @@ import sys
 
 if __name__ == "__main__":
 
-
     try:
         
-    command = sys.argv[1]
+        command = sys.argv[1]
 
-    if command == "l":
+        if command == "list":
 
-        print("list records")
+            print("list records")
+        
+        elif command == "get":
+
+            print("Get command")
+
+        elif command == "update":
+
+            print("updatecommand")
+        
+        elif command == "delete":
+
+            print("delete command")
+        else:
+            
+            prinf("No command is found :-(")
 
     except:
         print("error :-)")
